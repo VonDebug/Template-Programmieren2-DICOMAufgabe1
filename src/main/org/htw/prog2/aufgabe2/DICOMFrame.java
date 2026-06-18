8bec9c16cc65092de67b96b1672bacf62ce677e7
VonDebug <adrian.maroviqi@gmx.net>
2026-06-18 21:09:08 +0200

    Implement edge detection and frame parsing for DICOM images

--- src/main/org/htw/prog2/aufgabe2/DICOMFrame.java
+++ src/main/org/htw/prog2/aufgabe2/DICOMFrame.java
@@ -1,27 +1,77 @@
 package org.htw.prog2.aufgabe2;
 
-import java.awt.*;
+
 import java.awt.image.BufferedImage;
 
 public class DICOMFrame {
-    private static final double[][] S_x = new double[][]{new double[]{-1,0,1}, new double[]{-2,0,2}, new double[]{-1,0,1}};
-    private static final double[][] S_y = new double[][]{new double[]{-1,-2,-1}, new double[]{0,0,0}, new double[]{1,2,1}};
+    private static final int[][] S_x = new int[][]{new int[]{-1,0,1}, new int[]{-2,0,2}, new int[]{-1,0,1}};
+    private static final int[][] S_y = new int[][]{new int[]{-1,-2,-1}, new int[]{0,0,0}, new int[]{1,2,1}};
+    private final BufferedImage rawImage;
+    private BufferedImage imageEdges;
+    private double brightness;
+    private boolean detectEdgesCompleted = false;
 
     public DICOMFrame(BufferedImage image) {
+    this.rawImage = image;
+
+
+
     }
 
     private int getGrayscalePixel(BufferedImage image, int x, int y) {
-        return 0;
+        int color = image.getRGB(x, y);
+
+        int blue = color & 0xff;
+        int green = (color & 0xff00) >> 8;
+        int red = (color & 0xff0000) >> 16;
+
+        int grayscalePixel = (int) (0.2126*red + 0.7152*green + 0.0722*blue);
+
+        return grayscalePixel;
     }
 
     private void detectEdges() {
+
+        int width = this.rawImage.getWidth();
+        int height = this.rawImage.getHeight();
+        BufferedImage edgeImg = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+
+        for(int y = 0; y < height; y++){
+
+            for(int x = 0; x < width; x++) {
+
+                int pixelValueX =
+                        S_x[0][0] * getGrayscalePixel(this.rawImage, x, y) + S_x[0][1] * getGrayscalePixel(this.rawImage, x + 1, y) + S_x[0][2] * getGrayscalePixel(this.rawImage, x + 2, y) +
+                                S_x[1][0] * getGrayscalePixel(this.rawImage, x, y + 1) + S_x[1][1] * getGrayscalePixel(this.rawImage, x + 1, y + 1) + S_x[1][2] * getGrayscalePixel(this.rawImage, x + 2, y + 1) +
+                                S_x[2][0] * getGrayscalePixel(this.rawImage, x, y + 2) + S_x[2][1] * getGrayscalePixel(this.rawImage, x + 1, y + 2) + S_x[2][2] * getGrayscalePixel(this.rawImage, x + 2, y + 2);
+
+                int pixelValueY =
+                        S_y[0][0] * getGrayscalePixel(this.rawImage, x, y) + S_y[0][1] * getGrayscalePixel(this.rawImage, x + 1, y) + S_y[0][2] * getGrayscalePixel(this.rawImage, x + 2, y) +
+                                S_y[1][0] * getGrayscalePixel(this.rawImage, x, y + 1) + S_y[1][1] * getGrayscalePixel(this.rawImage, x + 1, y + 1) + S_y[1][2] * getGrayscalePixel(this.rawImage, x + 2, y + 1) +
+                                S_y[2][0] * getGrayscalePixel(this.rawImage, x, y + 2) + S_y[2][1] * getGrayscalePixel(this.rawImage, x + 1, y + 2) + S_y[2][2] * getGrayscalePixel(this.rawImage, x + 2, y + 2);
+
+                int sum = Math.abs(pixelValueX) + Math.abs(pixelValueY);
+
+                if (sum > 255) {
+                    sum = 255;
+                }
+
+
+            }
+        }
+
     }
 
     public BufferedImage getImage() {
-        return null;
+        return this.rawImage;
     }
 
     public BufferedImage getEdges(double brightness) {
-        return null;
+        if(!this.detectEdgesCompleted || this.detectEdgesCompleted && brightness != this.brightness) {
+            this.brightness = brightness;
+            detectEdges();
+        }
+
+        return this.imageEdges;
     }
 }
--- src/main/org/htw/prog2/aufgabe2/DICOMImage.java
+++ src/main/org/htw/prog2/aufgabe2/DICOMImage.java
@@ -1,18 +1,40 @@
 package org.htw.prog2.aufgabe2;
 
-import org.dcm4che3.imageio.plugins.dcm.DicomImageReadParam;
 
-import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
-import javax.imageio.stream.ImageInputStream;
+import com.pixelmed.dicom.AttributeList;
+import com.pixelmed.dicom.DicomException;
+import com.pixelmed.display.SourceImage;
+
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
+
 
 public class DICOMImage {
 
-    public DICOMImage(File infile, String name) {
+    public String name;
+    public  DICOMFrame[] allDicomFrames;
+
+    public DICOMImage(File infile, String name) throws DicomException, IOException {
+        this.name = name;
+
+        int countOfImages;
+
+       AttributeList attributeList = new AttributeList();
+       attributeList.read(infile);
+
+        SourceImage sourceImage = new SourceImage(attributeList);
+        countOfImages = sourceImage.getNumberOfBufferedImages();
+        this.allDicomFrames = new DICOMFrame[countOfImages];
+
+        for(int i = 0; i<countOfImages; i++){
+
+            DICOMFrame dicomFrame = new DICOMFrame(sourceImage.getBufferedImage(i));
+            this.allDicomFrames[i] = dicomFrame;
+        }
+
+
+
     }
 
     public void writeFrames(int from, int to, boolean original, boolean edges, double edgeLightnessCutoff) {
