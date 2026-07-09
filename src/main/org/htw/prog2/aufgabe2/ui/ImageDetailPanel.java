237098afd76046770e9646b09d8932ea26b07da4
VonDebug <adrian.maroviqi@gmx.net>
2026-07-09 18:28:27 +0200

    Update `ImageDetailPanel` to enhance UI layout and integrate detailed DICOM frame display functionality

--- src/main/org/htw/prog2/aufgabe2/ui/ImageDetailPanel.java
+++ src/main/org/htw/prog2/aufgabe2/ui/ImageDetailPanel.java
@@ -1,8 +1,12 @@
 package org.htw.prog2.aufgabe2.ui;
 
+import org.htw.prog2.aufgabe2.logic.DICOMFrame;
+
 import javax.swing.*;
 import javax.swing.border.Border;
+
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class ImageDetailPanel extends JPanel {
 
@@ -11,19 +15,36 @@ public class ImageDetailPanel extends JPanel {
     public ImageDetailPanel(){
         this.jPanel = new JPanel();
 
-        jPanel.setLayout(new BoxLayout(jPanel, BoxLayout.Y_AXIS));
+        jPanel.setLayout(new BorderLayout());
 
         this.jPanel.setBorder(BorderFactory.createEmptyBorder(5,15,0,0));
 
 
-
         JLabel jLabel = new JLabel("Detailansicht");
         jLabel.setFont(new Font("Arial", Font.BOLD, 20 ));
+        this.jPanel.add(jLabel, BorderLayout.NORTH);
 
-        jPanel.add(jLabel);
+            JLabel noImages = new JLabel("Keine Bildserie ausgewählt");
+            noImages.setFont(new Font("Arial", Font.BOLD, 30 ));
+            this.jPanel.add(noImages, BorderLayout.CENTER);
+        }
+
+        public void setDetailedFrame(DICOMFrame frame, boolean showEdges){
+
+            BufferedImage bufferedImage;
+
+        if(showEdges){
+            bufferedImage = frame.getEdges(10.0);
+        }
+        else{
+            bufferedImage = frame.getImage();
+        }
+        Graphics2D g = bufferedImage.createGraphics();
 
 
-    }
+
+        }
+
 
 
     public JPanel getjPanel() {
